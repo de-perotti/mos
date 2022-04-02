764d48a9b0cb8c9eb08d595ec453279f80949fd4
Luigi Perotti <perottilds@gmail.com>
2022-04-02 15:15:35 -0300

    chore: add offline page

---
+++ pages/_offline.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const Offline = () => {
+    return (
+        <>
+            <h1>perotti.dev</h1>
+            <p>Você está offline</p>
+        </>
+    );
+};
+
+export default Offline;
