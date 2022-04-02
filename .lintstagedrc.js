fe3d689e942bd9e8abeb1b867b043d80ce6c3ddc
Luigi Perotti <perottilds@gmail.com>
2022-04-02 13:19:18 -0300

    chore: first commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+    `next lint --fix --file ${filenames
+        .map((f) => path.relative(process.cwd(), f))
+        .join(' --file ')}`
+
+/**
+ * @type {{"*.{js,jsx,ts,tsx}": (function(*): string)[]}}
+ */
+module.exports = {
+    '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa')
 
-module.exports = nextConfig
+module.exports = withPWA(/** @type {import('next').NextConfig} */({
+  pwa: {
+    dest: 'public'
+  },
+  reactStrictMode: true,
+
+}))
